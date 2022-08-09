2822e7b981525fc1d7cef9b03574768776a1838a
YoungJinGo <skyyj32@naver.com>
2022-08-09 23:31:39 +0900

    canvas Background Setting

--- main.js
+++ main.js
@@ -9,3 +9,42 @@ canvas.width = 400;
 canvas.height = 700;
 
 document.body.append(canvas);
+
+
+
+let backgroundImage,bulletImage,characterImage,gameOverImage,enemyImage;
+
+// 캐릭터 좌표
+let charX;
+let charY;
+
+function loadImage() {
+	backgroundImage = new Image();
+	backgroundImage.src = "images/background.jpg";
+
+	bulletImage = new Image();
+	bulletImage.src = "images/bullet.png"
+
+	characterImage = new Image();
+	characterImage.src = "images/character.png";
+
+	enemyImage = new Image();
+	enemyImage.src = "images/enemy.png"
+
+	gameOverImage = new Image();
+	gameOverImage.src = "images/gameOver.png";
+}
+
+function render() {
+	ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+}
+
+function main() {
+
+	render();
+	requestAnimationFrame(main);
+}
+
+
+loadImage();
+main();
