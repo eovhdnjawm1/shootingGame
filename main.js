6db34024ce6c174f1459279375f881801ce9b539
YoungJinGo <skyyj32@naver.com>
2022-08-14 22:31:43 +0900

    update 함수화

--- main.js
+++ main.js
@@ -40,31 +40,43 @@ function render() {
 	ctx.drawImage(characterImage, charX, charY);
 }
 
-function main() {
 
-	render();
-	setupKeyboardListener();
-	requestAnimationFrame(main);
-}
 
 // 중복 키 방지를 위한것
 let keyDown = {}
 
 function setupKeyboardListener() {
 	document.addEventListener("keydown", (event) => {
-		console.log("이러한 키다", event.keyCode);
-		console.log("x의 좌표", charX)
 		keyDown[event.keyCode] = true;
-		if (event.keyCode === 37 && charX >= 0){
-			charX -= .05;
-		}
-		else if (event.keyCode === 39 && charX <= 350) {
-			charX += .05;
-		}
-		keyDown = {};
+		// if (event.keyCode === 37 && charX >= 0){
+		// 	charX -= .05;
+		// }
+		// else if (event.keyCode === 39 && charX <= 350) {
+		// 	charX += .05;
+		// }
+		document.addEventListener("keyup", (event) => {
+			delete keyDown[event.keyCode];
+		})
 	});
 }
 
+
+function update() {
+	if ( 37 in keyDown && charX >= 0){
+		charX -= 5;
+	}
+	else if ( 39 in keyDown && charX <= 350) {
+		charX += 5;
+	}
+}
+
+function main() {
+
+	update();
+	render();
+	setupKeyboardListener();
+	requestAnimationFrame(main);
+}
 
 loadImage();
 main();
